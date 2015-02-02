49cd1f73d251ea7aef98a9baff2e328afdffec28
nanakira <nanakira@users.noreply.github.com>
2015-02-02 15:11:41 +0900

    Add TurnOn test

--- SandBox/include/LedDriver/LedDriver.h
+++ SandBox/include/LedDriver/LedDriver.h
@@ -29,7 +29,8 @@
 
 #include <stdint.h>
 
-void LedDriver_Create(uint16_t*);
+void LedDriver_Create(uint16_t* address);
 void LedDriver_Destroy(void);
+void LedDriver_TurnOn(int ledNumber);
 
 #endif  /* D_LedDriver_H */
--- SandBox/src/LedDriver/LedDriver.c
+++ SandBox/src/LedDriver/LedDriver.c
@@ -31,10 +31,13 @@
 
 void LedDriver_Create(uint16_t* address)
 {
+	*address = 0;
 }
 
 void LedDriver_Destroy(void)
 {
 }
 
-
+void LedDriver_TurnOn(int ledNumber)
+{
+}
