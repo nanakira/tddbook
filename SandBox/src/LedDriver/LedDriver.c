e708f3506b4fa079200634c06b2c750ff2a8caa8
nanakira <nanakira@users.noreply.github.com>
2015-02-02 16:41:19 +0900

    Consider HW not readable

--- SandBox/src/LedDriver/LedDriver.c
+++ SandBox/src/LedDriver/LedDriver.c
@@ -35,10 +35,12 @@ enum {
 };
 
 static uint16_t* ledsAddress;
+static uint16_t ledsImage;
 
 void LedDriver_Create(uint16_t* address)
 {
 	ledsAddress = address;
+	ledsImage = ALL_LEDS_OFF;
 	*ledsAddress = ALL_LEDS_OFF;
 }
 
@@ -51,17 +53,25 @@ static uint16_t convertLedNumberToBit(int ledNumber)
 	return 1 << (ledNumber - 1);
 }
 
+static void updateHardware(void)
+{
+	*ledsAddress = ledsImage;
+
+}
 void LedDriver_TurnOn(int ledNumber)
 {
-	*ledsAddress |= convertLedNumberToBit(ledNumber);
+	ledsImage |= convertLedNumberToBit(ledNumber);
+	updateHardware();
 }
 
 void LedDriver_TurnAllOn(void)
 {
-	*ledsAddress = ALL_LEDS_ON;
+	ledsImage = ALL_LEDS_ON;
+	updateHardware();
 }
 
 void LedDriver_TurnOff(int ledNumber)
 {
-	*ledsAddress &= ~(convertLedNumberToBit(ledNumber));
+	ledsImage &= ~(convertLedNumberToBit(ledNumber));
+	updateHardware();
 }
