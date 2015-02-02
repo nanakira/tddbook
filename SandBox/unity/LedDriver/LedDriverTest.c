8eeded556a1f9fdfcc0a13cc7a6f9cd277810cde
nanakira <nanakira@users.noreply.github.com>
2015-02-02 15:29:17 +0900

    Refactor tests

--- SandBox/unity/LedDriver/LedDriverTest.c
+++ SandBox/unity/LedDriver/LedDriverTest.c
@@ -31,8 +31,11 @@
 
 TEST_GROUP(LedDriver);
 
+static uint16_t virtualLeds;
+
 TEST_SETUP(LedDriver)
 {
+	LedDriver_Create(&virtualLeds);
 }
 
 TEST_TEAR_DOWN(LedDriver)
@@ -41,23 +44,19 @@ TEST_TEAR_DOWN(LedDriver)
 
 TEST(LedDriver, LedsOffAfterCreate)
 {
-	uint16_t virtualLeds = 0xffff;
-	LedDriver_Create(&virtualLeds);
-	TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+	uint16_t localVirtualLeds = 0xffff;
+	LedDriver_Create(&localVirtualLeds);
+	TEST_ASSERT_EQUAL_HEX16(0, localVirtualLeds);
 }
 
 TEST(LedDriver, TurnOnLedOne)
 {
-	uint16_t virtualLeds;
-	LedDriver_Create(&virtualLeds);
 	LedDriver_TurnOn(1);
 	TEST_ASSERT_EQUAL_HEX16(1, virtualLeds);
 }
 
 TEST(LedDriver, TurnOffLedOne)
 {
-	uint16_t virtualLeds;
-	LedDriver_Create(&virtualLeds);
 	LedDriver_TurnOn(1);
 	LedDriver_TurnOff(1);
 	TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
