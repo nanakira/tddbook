a8be3dd55ff03983a25dea23758702cda7b6f342
nanakira <nanakira@users.noreply.github.com>
2015-02-02 15:14:53 +0900

    Make it green

--- SandBox/src/LedDriver/LedDriver.c
+++ SandBox/src/LedDriver/LedDriver.c
@@ -29,9 +29,12 @@
 #include <stdint.h>
 #include <memory.h>
 
+static uint16_t* ledsAddress;
+
 void LedDriver_Create(uint16_t* address)
 {
-	*address = 0;
+	ledsAddress = address;
+	*ledsAddress = 0;
 }
 
 void LedDriver_Destroy(void)
@@ -40,4 +43,5 @@ void LedDriver_Destroy(void)
 
 void LedDriver_TurnOn(int ledNumber)
 {
+	*ledsAddress = 1;
 }
